52f1c4122366d73c695244f9799ab2c4a0274555
tesfesminicar2020@gmail.com <tesfesminicar2020@gmail.com>
2024-08-15 15:53:02 +0900

    Mod run.py

--- run.py
+++ run.py
@@ -36,6 +36,105 @@ if config.HAVE_IMU: import gyro
 if config.HAVE_NN: import train_pytorch
 
 
+def measure_ultrasonic(d, ultrasonics):
+    # 認知（超音波センサ計測）
+    ## RrRHセンサ距離計測例：dis_RrRH = ultrasonic_RrRH.()
+    ## 下記では一気に取得
+    message = ""
+    for i, name in enumerate(config.ultrasonics_list):
+        d[i] = ultrasonics[name].measure()
+        #message += name + ":" + str(round(ultrasonics[name].dis,2)).rjust(7, ' ') #Thony表示用にprint変更
+        message += name + ":" + "{:>4}".format(round(ultrasonics[name].dis))+ ", "
+        # サンプリングレートを調整する場合は下記をコメントアウト外す
+        #time.sleep(sampling_cycle)
+    return message
+
+def planning_ultrasonic(plan, ultrasonics, model):
+        # 判断（プランニング）
+        # 使う超音波センサをconfig.pyのultrasonics_listで設定必要
+        ## ただ真っすぐに走る 
+        if config.mode_plan == "GoStraight":
+            steer_pwm_duty,throttle_pwm_duty = 0, config.FORWARD_S
+        ## 右左空いているほうに走る 
+        elif config.mode_plan == "Right_Left_3":
+            steer_pwm_duty,throttle_pwm_duty = plan.Right_Left_3(ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
+        ## 過去の値を使ってスムーズに走る
+        elif config.mode_plan == "Right_Left_3_Records":
+            steer_pwm_duty, throttle_pwm_duty  = plan.Right_Left_3_Records(ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
+        ## 右手法で走る
+        elif config.mode_plan == "RightHand":
+            steer_pwm_duty, throttle_pwm_duty  = plan.RightHand(ultrasonics["FrRH"].dis, ultrasonics["RrRH"].dis)
+        ## 左手法で走る
+        elif config.mode_plan == "LeftHand":
+            steer_pwm_duty, throttle_pwm_duty  = plan.LeftHand(ultrasonics["FrLH"].dis, ultrasonics["RrLH"].dis)
+        ## 右手法にPID制御を使ってスムーズに走る
+        elif config.mode_plan == "RightHand_PID":
+            steer_pwm_duty, throttle_pwm_duty  = plan.RightHand_PID(ultrasonics["FrRH"], ultrasonics["RrRH"])
+        ## 左手法にPID制御を使ってスムーズに走る
+        elif config.mode_plan == "LeftHand_PID":
+            steer_pwm_duty, throttle_pwm_duty  = plan.LeftHand_PID(ultrasonics["FrLH"], ultrasonics["RrLH"])
+        ## ニューラルネットを使ってスムーズに走る
+        #評価中
+        elif config.mode_plan == "NN":
+            # 超音波センサ入力が変更できるように引数をリストにして渡す形に変更
+            args = [ultrasonics[key].dis for key in config.ultrasonics_list]
+            steer_pwm_duty, throttle_pwm_duty = plan.NN(model, *args)
+            #steer_pwm_duty, throttle_pwm_duty  = plan.NN(model, ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
+        else: 
+            print("デフォルトの判断モードの選択ではありません, コードを書き換えてオリジナルのモードを実装しよう!")
+            # break
+        return steer_pwm_duty, throttle_pwm_duty
+
+def planning_detection():
+    pass
+
+def control_joystick(joystick, motor):
+    ## ジョイスティックで操作する場合は上書き
+    joystick.poll()
+    mode = joystick.mode[0]
+    if mode == "user":
+        steer_pwm_duty = int(joystick.steer*config.JOYSTICK_STEERING_SCALE*100)
+        throttle_pwm_duty = int(joystick.accel*config.JOYSTICK_THROTTLE_SCALE*100)
+        if joystick.accel2:
+            throttle_pwm_duty  = int(config.FORWARD_S)
+        elif joystick.accel1:
+            throttle_pwm_duty  = int(config.FORWARD_C)
+    elif mode == "auto_str":
+        throttle_pwm_duty = int(joystick.accel*config.JOYSTICK_THROTTLE_SCALE*100)
+        if joystick.accel2:
+            throttle_pwm_duty  = int(config.FORWARD_S)
+        elif joystick.accel1:
+            throttle_pwm_duty  = int(config.FORWARD_C)
+    if joystick.recording: 
+        recording = True
+    else: 
+        recording = False
+    ### コントローラでブレーキ
+    if joystick.breaking:
+        motor.breaking()
+    return steer_pwm_duty, throttle_pwm_duty, recording
+
+def object_detection(oakd_yolo, labels):
+    frame = None
+    try:
+        frame, detections = oakd_yolo.get_frame()
+    except BaseException:
+        print("===================")
+        print("get_frame() error! Reboot OAK-D.")
+        print("If reboot occur frequently, Bandwidth may be too much.")
+        print("Please lower FPS.")
+        print("===================")
+        # break
+    if (len(detections)) >= 1:
+        for detection in detections:
+            detection_label = labels[detection.label]
+            print(detection_label + "を検出しました。")
+
+    if frame is not None:
+        oakd_yolo.display_frame("nn", frame, detections)
+
+
+
 def main(args) -> None:
 
     # First Person Viewでの走行画像表示
@@ -68,6 +167,7 @@ def main(args) -> None:
     plan = planner.Planner(config.mode_plan)
 
     # NNモデルの読み込み
+    model = None
     if config.HAVE_NN:
         ## NNモデルの初期化
         ## 使う超音波センサの数、出力数、隠れ層の次元、隠れ層の数
@@ -113,96 +213,103 @@ def main(args) -> None:
     # ここから走行ループ
     try:
         while True:
+
             # 画像認識
-            frame = None
-            try:
-                frame, detections = oakd_yolo.get_frame()
-            except BaseException:
-                print("===================")
-                print("get_frame() error! Reboot OAK-D.")
-                print("If reboot occur frequently, Bandwidth may be too much.")
-                print("Please lower FPS.")
-                print("===================")
-                break
-            if (len(detections)) >= 1:
-                # print(detections)
-                for detection in detections:
-                    detection_label = labels[detection.label]
-                    print(detection_label + "を検出しました。")
+            object_detection(oakd_yolo, labels)
+            # frame = None
+            # try:
+            #     frame, detections = oakd_yolo.get_frame()
+            # except BaseException:
+            #     print("===================")
+            #     print("get_frame() error! Reboot OAK-D.")
+            #     print("If reboot occur frequently, Bandwidth may be too much.")
+            #     print("Please lower FPS.")
+            #     print("===================")
+            #     break
+            # if (len(detections)) >= 1:
+            #     for detection in detections:
+            #         detection_label = labels[detection.label]
+            #         print(detection_label + "を検出しました。")
 
-            if frame is not None:
-                oakd_yolo.display_frame("nn", frame, detections)
+            # if frame is not None:
+            #     oakd_yolo.display_frame("nn", frame, detections)
 
-            # 認知（計測）
-            ## RrRHセンサ距離計測例：dis_RrRH = ultrasonic_RrRH.()
-            ## 下記では一気に取得
-            message = ""
-            for i, name in enumerate(config.ultrasonics_list):
-                d[i] = ultrasonics[name].measure()
-                #message += name + ":" + str(round(ultrasonics[name].dis,2)).rjust(7, ' ') #Thony表示用にprint変更
-                message += name + ":" + "{:>4}".format(round(ultrasonics[name].dis))+ ", "
-                # サンプリングレートを調整する場合は下記をコメントアウト外す
-                #time.sleep(sampling_cycle)
+            # 認知（超音波センサ計測）
+            message = measure_ultrasonic(d, ultrasonics)
+
+            # ## RrRHセンサ距離計測例：dis_RrRH = ultrasonic_RrRH.()
+            # ## 下記では一気に取得
+            # message = ""
+            # for i, name in enumerate(config.ultrasonics_list):
+            #     d[i] = ultrasonics[name].measure()
+            #     #message += name + ":" + str(round(ultrasonics[name].dis,2)).rjust(7, ' ') #Thony表示用にprint変更
+            #     message += name + ":" + "{:>4}".format(round(ultrasonics[name].dis))+ ", "
+            #     # サンプリングレートを調整する場合は下記をコメントアウト外す
+            #     #time.sleep(sampling_cycle)
 
             # 判断（プランニング）
-            # 使う超音波センサをconfig.pyのultrasonics_listで設定必要
-            ## ただ真っすぐに走る 
-            if config.mode_plan == "GoStraight":
-                steer_pwm_duty,throttle_pwm_duty = 0, config.FORWARD_S
-            ## 右左空いているほうに走る 
-            elif config.mode_plan == "Right_Left_3":
-                steer_pwm_duty,throttle_pwm_duty = plan.Right_Left_3(ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
-            ## 過去の値を使ってスムーズに走る
-            elif config.mode_plan == "Right_Left_3_Records":
-                steer_pwm_duty, throttle_pwm_duty  = plan.Right_Left_3_Records(ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
-            ## 右手法で走る
-            elif config.mode_plan == "RightHand":
-                steer_pwm_duty, throttle_pwm_duty  = plan.RightHand(ultrasonics["FrRH"].dis, ultrasonics["RrRH"].dis)
-            ## 左手法で走る
-            elif config.mode_plan == "LeftHand":
-                steer_pwm_duty, throttle_pwm_duty  = plan.LeftHand(ultrasonics["FrLH"].dis, ultrasonics["RrLH"].dis)
-            ## 右手法にPID制御を使ってスムーズに走る
-            elif config.mode_plan == "RightHand_PID":
-                steer_pwm_duty, throttle_pwm_duty  = plan.RightHand_PID(ultrasonics["FrRH"], ultrasonics["RrRH"])
-            ## 左手法にPID制御を使ってスムーズに走る
-            elif config.mode_plan == "LeftHand_PID":
-                steer_pwm_duty, throttle_pwm_duty  = plan.LeftHand_PID(ultrasonics["FrLH"], ultrasonics["RrLH"])
-            ## ニューラルネットを使ってスムーズに走る
-            #評価中
-            elif config.mode_plan == "NN":
-                # 超音波センサ入力が変更できるように引数をリストにして渡す形に変更
-                args = [ultrasonics[key].dis for key in config.ultrasonics_list]
-                steer_pwm_duty, throttle_pwm_duty = plan.NN(model, *args)
-                #steer_pwm_duty, throttle_pwm_duty  = plan.NN(model, ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
-            else: 
-                print("デフォルトの判断モードの選択ではありません, コードを書き換えてオリジナルのモードを実装しよう!")
-                break
+            steer_pwm_duty, throttle_pwm_duty = planning_ultrasonic(plan, ultrasonics, model)
+            # # 使う超音波センサをconfig.pyのultrasonics_listで設定必要
+            # ## ただ真っすぐに走る 
+            # if config.mode_plan == "GoStraight":
+            #     steer_pwm_duty,throttle_pwm_duty = 0, config.FORWARD_S
+            # ## 右左空いているほうに走る 
+            # elif config.mode_plan == "Right_Left_3":
+            #     steer_pwm_duty,throttle_pwm_duty = plan.Right_Left_3(ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
+            # ## 過去の値を使ってスムーズに走る
+            # elif config.mode_plan == "Right_Left_3_Records":
+            #     steer_pwm_duty, throttle_pwm_duty  = plan.Right_Left_3_Records(ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
+            # ## 右手法で走る
+            # elif config.mode_plan == "RightHand":
+            #     steer_pwm_duty, throttle_pwm_duty  = plan.RightHand(ultrasonics["FrRH"].dis, ultrasonics["RrRH"].dis)
+            # ## 左手法で走る
+            # elif config.mode_plan == "LeftHand":
+            #     steer_pwm_duty, throttle_pwm_duty  = plan.LeftHand(ultrasonics["FrLH"].dis, ultrasonics["RrLH"].dis)
+            # ## 右手法にPID制御を使ってスムーズに走る
+            # elif config.mode_plan == "RightHand_PID":
+            #     steer_pwm_duty, throttle_pwm_duty  = plan.RightHand_PID(ultrasonics["FrRH"], ultrasonics["RrRH"])
+            # ## 左手法にPID制御を使ってスムーズに走る
+            # elif config.mode_plan == "LeftHand_PID":
+            #     steer_pwm_duty, throttle_pwm_duty  = plan.LeftHand_PID(ultrasonics["FrLH"], ultrasonics["RrLH"])
+            # ## ニューラルネットを使ってスムーズに走る
+            # #評価中
+            # elif config.mode_plan == "NN":
+            #     # 超音波センサ入力が変更できるように引数をリストにして渡す形に変更
+            #     args = [ultrasonics[key].dis for key in config.ultrasonics_list]
+            #     steer_pwm_duty, throttle_pwm_duty = plan.NN(model, *args)
+            #     #steer_pwm_duty, throttle_pwm_duty  = plan.NN(model, ultrasonics["FrLH"].dis, ultrasonics["Fr"].dis, ultrasonics["FrRH"].dis)
+            # else: 
+            #     print("デフォルトの判断モードの選択ではありません, コードを書き換えてオリジナルのモードを実装しよう!")
+            #     break
 
             # 操作（ステアリング、アクセル）
-            ## ジョイスティックで操作する場合は上書き
             if config.HAVE_CONTROLLER:
-                joystick.poll()
-                mode = joystick.mode[0]
-                if mode == "user":
-                    steer_pwm_duty = int(joystick.steer*config.JOYSTICK_STEERING_SCALE*100)
-                    throttle_pwm_duty = int(joystick.accel*config.JOYSTICK_THROTTLE_SCALE*100)
-                    if joystick.accel2:
-                        throttle_pwm_duty  = int(config.FORWARD_S)
-                    elif joystick.accel1:
-                        throttle_pwm_duty  = int(config.FORWARD_C)
-                elif mode == "auto_str":
-                    throttle_pwm_duty = int(joystick.accel*config.JOYSTICK_THROTTLE_SCALE*100)
-                    if joystick.accel2:
-                        throttle_pwm_duty  = int(config.FORWARD_S)
-                    elif joystick.accel1:
-                        throttle_pwm_duty  = int(config.FORWARD_C)
-                if joystick.recording: 
-                    recording = True
-                else: 
-                    recording = False
-                ### コントローラでブレーキ
-                if joystick.breaking:
-                    motor.breaking()
+                steer_pwm_duty, throttle_pwm_duty, recording = control_joystick(joystick, motor)
+
+            # ## ジョイスティックで操作する場合は上書き
+            # if config.HAVE_CONTROLLER:
+            #     joystick.poll()
+            #     mode = joystick.mode[0]
+            #     if mode == "user":
+            #         steer_pwm_duty = int(joystick.steer*config.JOYSTICK_STEERING_SCALE*100)
+            #         throttle_pwm_duty = int(joystick.accel*config.JOYSTICK_THROTTLE_SCALE*100)
+            #         if joystick.accel2:
+            #             throttle_pwm_duty  = int(config.FORWARD_S)
+            #         elif joystick.accel1:
+            #             throttle_pwm_duty  = int(config.FORWARD_C)
+            #     elif mode == "auto_str":
+            #         throttle_pwm_duty = int(joystick.accel*config.JOYSTICK_THROTTLE_SCALE*100)
+            #         if joystick.accel2:
+            #             throttle_pwm_duty  = int(config.FORWARD_S)
+            #         elif joystick.accel1:
+            #             throttle_pwm_duty  = int(config.FORWARD_C)
+            #     if joystick.recording: 
+            #         recording = True
+            #     else: 
+            #         recording = False
+            #     ### コントローラでブレーキ
+            #     if joystick.breaking:
+            #         motor.breaking()
 
             ## モータードライバーに出力をセット
             ### 補正（動的制御）
@@ -296,7 +403,6 @@ def main(args) -> None:
 
 
 if __name__ == "__main__":
-
     # 引数設定
     parser = argparse.ArgumentParser()
     parser.add_argument(
